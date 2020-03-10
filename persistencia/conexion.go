6131f3f413e10d4069b614d3572d0b664e1d35d4
Rogelio <rogelio.consejo@gmail.com>
2020-03-10 14:28:40 -0600

    persistencia/conexion

---
+++ persistencia/conexion.go
@@ -0,0 +1,26 @@
+package persistencia
+
+import (
+"database/sql"
+"fmt"
+_ "github.com/go-sql-driver/mysql"
+)
+
+//CredencialesSQL: el nombre de usuario y password para conectarse a la base de datos.
+type CredencialesSQL struct {
+	User     string
+	Password string
+}
+
+//ConectarMySQL crea una conexión a una base de datos MySQL.
+func ConectarMySQL(credenciales CredencialesSQL, nombreDeBaseDeDatos string) (*sql.DB, error) {
+	datosParaConectarse := fmt.Sprintf("%s:%s@/%s?parseTime=true", credenciales.User, credenciales.Password, nombreDeBaseDeDatos)
+
+	baseDeDatos, err := sql.Open("mysql", datosParaConectarse)
+	if err != nil {
+		mensajeDeError := fmt.Sprintf("No se pudo conectar a la base de datos (%s)", nombreDeBaseDeDatos)
+		return nil, fmt.Errorf(mensajeDeError)
+	}
+
+	return baseDeDatos, nil
+}
