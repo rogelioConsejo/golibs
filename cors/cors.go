6571456018d76b4b5395f7085aa2605fac5572a9
rogelioConsejo <rogelio.consejo@gmail.com>
2023-03-01 20:09:23 +0100

    feat: basic CORS

---
+++ cors/cors.go
@@ -0,0 +1,25 @@
+package cors
+
+import (
+	"net/http"
+	"strings"
+)
+
+func EnableCORS(h http.Handler, methods ...string) http.Handler {
+	return corsEnabledHandler{handler: h, methods: methods}
+}
+
+type corsEnabledHandler struct {
+	handler http.Handler
+	methods []string
+}
+
+func (c corsEnabledHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodOptions {
+		writer.Header().Set("Access-Control-Allow-Origin", "*")
+		writer.Header().Set("Access-Control-Allow-Methods", strings.Join(c.methods, ","))
+		writer.WriteHeader(http.StatusOK)
+		return
+	}
+	c.handler.ServeHTTP(writer, request)
+}
