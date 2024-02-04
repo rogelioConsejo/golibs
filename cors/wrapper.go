35768e40109f15b408f2516ff84cf73b515469b7
rogelioConsejo <rogelio.consejo@gmail.com>
2024-02-04 15:14:20 -0600

    refactor: improve documentation

--- cors/wrapper.go
+++ cors/wrapper.go
@@ -1,7 +1,9 @@
+// Package cors provides a middleware that easily enables CORS for existing handlers.
 package cors
 
 import "net/http"
 
+// NewWrapper returns a new Wrapper. The implementation is abstracted and encapsulated in the returned value.
 func NewWrapper() Wrapper {
 	return &wrapper{}
 }
@@ -10,13 +12,9 @@ func NewWrapper() Wrapper {
 // It allows to set the origin, the methods and if credentials are allowed
 // The wrap configuration must be done before calling Wrap
 type Wrapper interface {
-	// Wrap returns a new http.Handler that wraps the given handler and enables CORS with the given configuration
 	Wrap(http.Handler) http.Handler
-	// SetOrigin sets the origin for the CORS configuration to the given URL, otherwise it will default to *
 	SetOrigin(URL)
-	// AddMethod adds a method to the CORS configuration
 	AddMethod(s Method)
-	// SetAllowCredentials sets the CORS configuration to allow credentials
 	SetAllowCredentials()
 }
 
@@ -32,10 +30,12 @@ type wrapper struct {
 	allowsCredentials bool
 }
 
+// SetAllowCredentials sets the CORS configuration to allow credentials
 func (w *wrapper) SetAllowCredentials() {
 	w.allowsCredentials = true
 }
 
+// AddMethod adds a Method to the CORS wrapper configuration
 func (w *wrapper) AddMethod(m Method) {
 	if w.methods == nil {
 		w.methods = make(map[Method]bool)
@@ -43,10 +43,12 @@ func (w *wrapper) AddMethod(m Method) {
 	w.methods[m] = true
 }
 
+// SetOrigin sets the origin for the CORS configuration to the given URL, otherwise it will default to *
 func (w *wrapper) SetOrigin(o URL) {
 	w.origin = o
 }
 
+// Wrap returns a new http.Handler that wraps the given handler and enables CORS with the given configuration
 func (w *wrapper) Wrap(h http.Handler) http.Handler {
 	var methods []string
 	for m := range w.methods {
